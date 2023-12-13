a7a56c4661a6f993731447af9d466e94c700d233
darkmian939 <cerikdamian@gmail.com>
2023-12-13 15:04:55 -0500

    actualizaditox4 todillo

---
+++ frontendparqueando/frontendparqueando/Controllers/CalificacionController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationParqueando.Models.DTO;
+using WebApplicationParqueando.Repository.Interfaces;
+using WebApplicationParqueando.Utilities;
+
+namespace WebApplicationBilling.Controllers
+{
+    public class CalificacionController : Controller
+    {
+        private readonly ICalificacionRepository _calificacionRepository;
+
+        public CalificacionController(ICalificacionRepository calificacionRepository)
+        {
+            this._calificacionRepository = calificacionRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new CalificacionDTO() { });
+        }
+
+        public async Task<IActionResult> GetAllCalificaciones()
+        {
+            try
+            {
+                var data = await _calificacionRepository.GetAllAsync(UrlResources.UrlBase + UrlResources.UrlCalificaciones);
+                return Json(new { data });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+
+        public async Task<IActionResult> GetCalificacionById(int id)
+        {
+            try
+            {
+                var data = await _calificacionRepository.GetByIdAsync(id);
+                return Json(new { data });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCalificacion([FromBody] CalificacionDTO calificacion)
+        {
+            try
+            {
+                var result = await _calificacionRepository.AddAsync(calificacion);
+                return Json(new { data = result });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCalificacion([FromBody] CalificacionDTO calificacion)
+        {
+            try
+            {
+                var result = await _calificacionRepository.UpdateAsync(calificacion);
+                return Json(new { data = result });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCalificacion(int id)
+        {
+            try
+            {
+                var result = await _calificacionRepository.DeleteAsync(id);
+                return Json(new { data = result });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+    }
+}
---
+++ frontendparqueando/frontendparqueando/Controllers/ComentarioController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationParqueando.Models.DTO;
+using WebApplicationParqueando.Repository.Interfaces;
+using WebApplicationParqueando.Utilities;
+
+namespace WebApplicationBilling.Controllers
+{
+    public class ComentarioController : Controller
+    {
+        private readonly IComentarioRepository _comentarioRepository;
+
+        public ComentarioController(IComentarioRepository comentarioRepository)
+        {
+            this._comentarioRepository = comentarioRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new ComentarioDTO() { });
+        }
+
+        public async Task<IActionResult> GetAllComentarios()
+        {
+            try
+            {
+                var data = await _comentarioRepository.GetAllAsync(UrlResources.UrlBase + UrlResources.UrlComentarios);
+                return Json(new { data });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+
+        public async Task<IActionResult> GetComentarioById(int id)
+        {
+            try
+            {
+                var data = await _comentarioRepository.GetByIdAsync(id);
+                return Json(new { data });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComentario([FromBody] ComentarioDTO comentario)
+        {
+            try
+            {
+                var result = await _comentarioRepository.AddAsync(comentario);
+                return Json(new { data = result });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateComentario([FromBody] ComentarioDTO comentario)
+        {
+            try
+            {
+                var result = await _comentarioRepository.UpdateAsync(comentario);
+                return Json(new { data = result });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteComentario(int id)
+        {
+            try
+            {
+                var result = await _comentarioRepository.DeleteAsync(id);
+                return Json(new { data = result });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error. Please try again later.");
+            }
+        }
+    }
+}
